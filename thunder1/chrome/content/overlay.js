d01e61dd60415937f3fadba8c51f324819f7a133
Luciano Marfil <luchomarfil@gmail.com>
2015-09-29 17:15:16 -0300

    Commit inicial

---
+++ thunder1/chrome/content/overlay.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function(e) { 
+	startup(); 
+}, false);
+
+window.setInterval(
+	function() {
+		startup(); 
+	}, 60000); //update date every minute
+
+function startup() {
+    console.log("prueba");
+  	var myPanel = document.getElementById("my-panel");
+  	var date = new Date();
+  	var day = date.getDay();
+  	var dateString = date.getFullYear() + "." + (date.getMonth()+1) + "." + date.getDate();
+  	myPanel.label = "Date: " + dateString;
+}
