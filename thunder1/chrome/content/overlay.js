62c2911cc7d313626be85605323e44143b6fd836
Luciano Marfil <luchomarfil@gmail.com>
2015-10-02 16:19:02 -0300

    Avances

--- thunder1/chrome/content/overlay.js
+++ thunder1/chrome/content/overlay.js
@@ -1,35 +1,15 @@
-Cu.import("resource:///modules/gloda/log4moz.js");
-
-function setupLogging() {
-  // The basic formatter will output lines like:
-  // DATE/TIME	LoggerName	LEVEL	(log message) 
-  let formatter = new Log4Moz.BasicFormatter();
-
-  // Loggers are hierarchical, lowering this log level will affect all output
-  let root = Log4Moz.repository.rootLogger;
-  root.level = Log4Moz.Level["All"];
-
-  // A console appender outputs to the JS Error Console
-  let capp = new Log4Moz.ConsoleAppender(formatter);
-  capp.level = Log4Moz.Level["Warn"];
-  root.addAppender(capp);
-
-  // A dump appender outputs to standard out
-  let dapp = new Log4Moz.DumpAppender(formatter);
-  dapp.level = Log4Moz.Level["Debug"];
-  root.addAppender(dapp);
+function prepararPalabrasIgnoradas(){
+  this.palabrasIgnoradas = {};
+  this.palabrasIgnoradasArray = "nueva nuevo entre dentro a ante bajo cabe con contra de desde hasta para por segun sin sobre tras ustedes ellos nosotros ".split(" ");  
+  this.palabrasIgnoradasArray = this.palabrasIgnoradasArray.concat("dólar además asimismo del mismo modo de la misma manera igualmente no solo sino también a causa como debido gracias por culpa porque puesto que visto dado ya menos con condición tal en caso si siempre suponiendo consecuencia así ahí entonces consiguiente esa razón eso es lo tanto fin el objetivo intención objeto para saber efecto otras palabras decir o sea ejemplo pesar al contrario aunque cambio comparación comparado mientras obstante pero sin embargo ante todo antes nada después cual primer segundo último lugar finalmente luego concluir empezar terminar otra parte otro lado una un primero sobre y sucesivamente demás respecto propósito he dicho se mencionó anteriormente relación relativo cuanto ese respecta según mostrado conclusión definitiva pocas resumen suma resumir partir actualmente ahora final principio apenas tan pronto cuando desde durante nuestros días tiempo enseguida hasta hoy día más tarde vez".split(" "));
+  for each (var item in palabrasIgnoradasArray) {
+      this.palabrasIgnoradas[item] = true;
+  }
 }
 
-
-
-let logger = Log4Moz.repository.getLogger("MyExtension.MyClass");
-logger.level = Log4Moz.Level["Debug"];
-
-// Log some messages
-// Given our settings, the error would show up everywhere, but the
-// debug one would only show up in stdout
-logger.error("Oh noes!! Something bad happened!");
-logger.debug("Details about bad thing only useful during debugging");
+this.minimoRepeticiones = 50;
+this.minimoTamanioPalabra = 5;
+this.prepararPalabrasIgnoradas();
 
 
 
@@ -59,11 +39,20 @@ function contarPalabras(){
 	tabmail.openTab("chromeTab", { chromePage: aURL })	
 }
 
+
 function analizarPalabras(){
+  window.setTimeout(function() {
+      analizarPalabrasAsynch();
+  }, 1000);  
+}
+
+
+function analizarPalabrasAsynch(){
 		//1 Obtengo todos los asuntos de rss
 		var acctMgr = Components.classes["@mozilla.org/messenger/account-manager;1"].getService(Components.interfaces.nsIMsgAccountManager);  
     var accounts = acctMgr.accounts; 
-		var cuentasRSS = []; 
+		var cuentasRSS = [];
+    this.listaPalabras = {} 
 		console.log("accounts: "+ accounts);
 		//se filtran solo las del tipo rss
     for (var i = 0; i < accounts.length; i++) {  
@@ -72,7 +61,6 @@ function analizarPalabras(){
 					  cuentasRSS.push(account);
 				 }
      }  
-		 
 		 //se imprimen los mensaes de esas cuentas
 		 for (var i = 0; i < cuentasRSS.length; i++) {  
 			  var account = cuentasRSS[i];
@@ -84,15 +72,46 @@ function analizarPalabras(){
 								 //   return account.QueryInterface(Components.interfaces.nsIMsgAccount);  
 							 var aFolder = subFolders.getNext().QueryInterface(Components.interfaces.nsIMsgFolder);
 							 var msgArray = aFolder.messages;  
-							 console.log("account:"+account.incomingServer.constructedPrettyName+" folder:"+aFolder.name);
+				//			 console.log("account:"+account.incomingServer.constructedPrettyName+" folder:"+aFolder.name);
 							 while( msgArray.hasMoreElements() ) {  
-									 let msgHdr = msgArray.getNext().QueryInterface(Components.interfaces.nsIMsgDBHdr);  
-									 let title = msgHdr.mime2DecodedSubject;  
-									 // do stuff with msgHdr  
-									 console.log("titulo:" + title);
+									 let msgHdr = msgArray.getNext().QueryInterface(Components.interfaces.nsIMsgDBHdr);  									 
+									 procesarMensaje(msgHdr);
 							 }	
 						}  
-				}
-		
+				}		
 		 }
+     
+     //aca obtengo todos las palabras utilizadas
+     //se ordenan por relevancia
+     this.palabrasOrdenadas = sortMapByValue(this.listaPalabras);
+     //se imprime en pantalla
+     //console.log(this.palabrasOrdenadas);     
+     $("p#wordsTracker-textOutput")[0].innerHTML = this.palabrasOrdenadas;
+     jQuery('#palabras-body').append('<tr><td class="pref-name">accessibility.</td><td class="pref-value">40</td></tr>');
 }
+
+function procesarMensaje(mensaje){
+    let title = mensaje.mime2DecodedSubject;  
+    //console.log("titulo:" + title);
+    var lista = title.replace(/[|&;$%@"<>()+,?!¿¡]/g, "").toLowerCase().split(" ");
+    //var lista = "hola como va, bien?".replace(/[|&;$%@"<>()+,?!¿¡]/g, "").toLowerCase().split(" ");    
+    for each (var item in lista) {
+        //solo palabras mayores a 4 caracteres
+        if(item.length >= this.minimoTamanioPalabra && !this.palabrasIgnoradas[item]){
+          this.listaPalabras[item] = this.listaPalabras[item] + 1 || 1;
+        }
+    }    
+}
+
+function sortMapByValue(map)
+{
+    var sortable = [];
+    for (var palabra in map){
+      if(map[palabra]>this.minimoRepeticiones ){
+          sortable.push([palabra, map[palabra]]);
+      }
+    }        
+    sortable.sort(function(a, b) {return b[1] - a[1]})
+    return sortable
+}
+  
